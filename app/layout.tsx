fd0cc469a4bba9d2d75986a96a2bd8c6295f7d5a
Philip <anlanderp@gmail.com>
2024-11-07 09:35:54 +0100

    link test font

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/qnl0pvt.css" />
+      </head>
       <body>
         <Header />
         {children}
