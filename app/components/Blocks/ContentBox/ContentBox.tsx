d017765e08eff194a452cf901f22aae24c6ba5c2
Philip <anlanderp@gmail.com>
2024-11-15 13:41:35 +0100

    content box

--- app/components/Blocks/ContentBox/ContentBox.tsx
+++ app/components/Blocks/ContentBox/ContentBox.tsx
@@ -4,7 +4,7 @@ import scss from './contentbox.module.scss'
 const ContentBox = ({ props }: any) => {
   return (
     <div className="flex bg-[#F0F0F0] max-w-[80%] mx-auto my-14 p-20 rounded-3xl">
-      <div className={`text-[18px] open-sans ${scss.contentBox}`}>{render(props.content)}</div>
+      <div className={`text-[18px] open-sans ${scss.contentBox} flex flex-col gap-5`}>{render(props.content)}</div>
     </div>
   );
 };
