389281ec149ac8a726d8e13893202113896088b0
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-02-11 11:03:28 +0100

    reworked partial payment

--- app/components/PartialPayment/partial-payment.tsx
+++ app/components/PartialPayment/partial-payment.tsx
@@ -1,15 +1,16 @@
 import { PiWarningCircleThin } from "react-icons/pi";
+import { IoWarningOutline } from "react-icons/io5";
 import { render } from "storyblok-rich-text-react-renderer";
 
 export const PartialPayment = ({ props }: any) => {
   return (
     <div className="flex justify-center p-4 lg:pb-16 lg:pt-0 w-[80%] lg:w-[80%] mx-auto mb-6 lg:mb-0 mt-8 lg:mt-0">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center gap-4">
         <div>
-          <PiWarningCircleThin fontSize={40} />
+          <IoWarningOutline fontSize={100} className="text-red-700" />
         </div>
         <div className="text-center">
-          <h2 className="font-bold pb-2 text-[20px]">{props.payment_title}</h2>
+          <h2 className="font-bold pb-4 text-[20px]">{props.payment_title}</h2>
           <div className="lg:w-[800px] partial-content">
             {render(props.payment_content)}
           </div>
